08446817a76f5b783f32e20d75c14025a843fb12
Guilherme Moretti <guimoretti2015@gmail.com>
2020-08-10 21:56:37 -0300

    commit aula 25/27

---
+++ Aula 25 - Depuração I.c
@@ -0,0 +1,6 @@
+/*
+    A aula é sobre depuração, tudo depende do seu editor de codigo/IDE.
+    Mas vou fazer a recomendação do editor de texto que uso, o Visual Studio Code
+    Recomendação : https://www.youtube.com/watch?v=qCxTBPEH8m4&t=1s
+    Att: Guilherme Moretti
+*/
---
+++ Aula 27 - Laços Condicionais I.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/*
+O laço de repetição do-while funciona de maneira semelhante ao laço de repetição "while".
+A diferença entre os dois é que o laço "while" só executa o bloco caso a condição seja verdadeira, enquanto que o laço de repetição do-while, ira primeiro
+executar o bloco de repetição e somente em seguida, irá verificar se a condição é ou não verdadeira.
+
+*/
+//! Pseudocodigo
+/*
+faça
+{
+}enquanto( true );
+*/
+
+void main()
+{
+char c;
+int contador = 0;
+do
+{
+    contador += 1;
+    printf("Digite 0 para sair do looping\n");
+    printf("Quantas vezes foi executado o looping: %i\n",contador);
+    c = getchar();
+    
+
+}
+while (c!='0');
+}
