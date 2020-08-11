031b74e8a2864378dd805e31faa41ba4a097fa17
Guilherme Moretti <guimoretti2015@gmail.com>
2020-08-10 22:22:49 -0300

    Update Aula 27 - Laços Condicionais I.c

--- Aula 27 - Laços Condicionais I.c
+++ Aula 27 - Laços Condicionais I.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+//!                     Links                  
+//? http://linguagemc.com.br/comando-do-while/
+//? https://www.youtube.com/watch?reload=9&v=1TcBY8aU-DE&list=PLesCEcYj003SwVdufCQM5FIbrOd0GG1M4&index=27
+//? http://excript.com/linguagem-c/estrutura-repeticao-do-while-c.html
+
+
 /*
 O laço de repetição do-while funciona de maneira semelhante ao laço de repetição "while".
 A diferença entre os dois é que o laço "while" só executa o bloco caso a condição seja verdadeira, enquanto que o laço de repetição do-while, ira primeiro
